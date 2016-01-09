83cc51f416c5c4a889e38f4769972701b36ef83b
Harleigh Campbell <harleighcampbell@gmail.com>
2016-01-09 23:33:16 +0000

    correctly added [ENV] in setup_mail.rd

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
   :address               => 'smtp.sendgrid.net',
   :port                  => '587',
   :authentication        => :plain,
-  :user_name             => [ENV]'app45840031@heroku.com',
-  :password              => [ENV]'awiduoj67802',
+  :user_name             => ENV['app45840031@heroku.com'],
+  :password              => ENV['awiduoj67802'],
   :domain                => 'heroku.com',
   :enable_starttls_auto  => true
 }
